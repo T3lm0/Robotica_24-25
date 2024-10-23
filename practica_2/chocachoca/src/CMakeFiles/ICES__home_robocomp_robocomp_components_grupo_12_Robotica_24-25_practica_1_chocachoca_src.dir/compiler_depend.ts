a1e4ab21e27dec8a7238c088d52e692250adac58
Tu Nombre <you@example.com>
2024-10-23 18:34:12 +0200

    Práctica 2

--- practica_2/chocachoca/src/CMakeFiles/ICES__home_robocomp_robocomp_components_grupo_12_Robotica_24-25_practica_1_chocachoca_src.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for ICES__home_robocomp_robocomp_components_grupo_12_Robotica_24-25_practica_1_chocachoca_src.
